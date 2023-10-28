4c826fceda2394b58730455e253815ce4c5168ba
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-10-28 19:13:30 -0300

    Add support to roboto fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+// roboto font
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+// global style
 import './globals.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
