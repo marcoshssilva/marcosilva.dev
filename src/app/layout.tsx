2be62d5e99307b8d7f96970ee03784a06c05715c
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-10-28 19:18:38 -0300

    clean page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
   )
