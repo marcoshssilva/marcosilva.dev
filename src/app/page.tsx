0557807a4fbaad471d10b38d237e6bb73e85dab3
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-12-21 22:36:18 -0300

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,25 +19,29 @@ export default function Home() {
       <Container>
         <Box className="min-h-screen flex flex-col items-start justify-center px-5">
 
-            <Typography className="pb-4" sx={{ width: '100%' }} variant={ isTablet ? 'h2' : 'h5'} align={ isTablet ? 'left' : 'center'}>Prazer, eu sou</Typography>
-            <Typography className="pb-4" sx={{ width: '100%' }} variant={ isTablet ? 'h1' : 'h3'} align={ isTablet ? 'left' : 'center'}>Marcos Henrique</Typography>
-            <Typography className="pb-4" sx={{ width: '100%' }} variant={ isTablet ? 'h2' : 'h3'} align={ isTablet ? 'left' : 'center'}>Desenvolvo aplicações para a web.</Typography>
+            <Typography className="pb-4" sx={{ width: '100%' }} variant={ isTablet ? 'h2' : 'h5'} align={ isTablet ? 'left' : 'center'}>
+              Olá, eu sou
+            </Typography>
+            <Typography className="pb-4" sx={{ width: '100%' }} variant={ isTablet ? 'h1' : 'h3'} align={ isTablet ? 'left' : 'center'}>
+              Marcos Henrique
+            </Typography>
+            <Typography className="pb-4" sx={{ width: '100%' }} variant={ isTablet ? 'h2' : 'h3'} align={ isTablet ? 'left' : 'center'}>
+              Desenvolvo aplicações para a web.
+            </Typography>
 
             <Box className="w-full flex justify-center md:justify-start">
+
               <Button 
                 startIcon={ <LinkedInIcon /> } 
                 href="https://www.linkedin.com/in/marcos-henrique-santana/" 
-                target="_blank">
-                LinkedIn
-              </Button>
+                target="_blank">LinkedIn</Button>
+
               <Button 
                 startIcon={ <GitHubIcon /> } 
                 href="https://github.com/marcoshssilva" 
-                target="_blank">
-                GitHub
-              </Button>
-            </Box>
-            
+                target="_blank">GitHub</Button>
+
+            </Box>            
 
         </Box>
       </Container>
