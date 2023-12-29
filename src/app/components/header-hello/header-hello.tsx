63a2e9b297d3bda46aa4cf42aca909eaccc24556
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-12-29 00:04:02 -0300

    Create component header-hello

---
+++ src/app/components/header-hello/header-hello.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function HeaderHelloComponent() {
+    return <>
+        <h1>Olá, eu sou</h1>
+        <h1>Marcos Henrique.</h1>
+        <h1>Desenvolvo aplicações para a web.</h1>
+        </>;
+}
