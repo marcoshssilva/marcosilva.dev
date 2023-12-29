bf3fa12744abb521c8c0561115167df82e17aea6
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-12-29 00:23:19 -0300

    Adjust size and font

--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -2,8 +2,8 @@ import { Typography } from "@mui/material";
 
 export default function HeaderHelloComponent() {
     return <>
-        <h1>Olá, eu sou</h1>
-        <h1>Marcos Henrique.</h1>
-        <h1>Desenvolvo aplicações para a web.</h1>
+        <h1 className={'font-semibold text-[64px]'}>Olá, eu sou</h1>
+        <h1 className={'font-semibold text-[64px]'}>Marcos Henrique.</h1>
+        <h1 className={'font-light text-[44px]'}>Desenvolvo aplicações para a web.</h1>
         </>;
 }
