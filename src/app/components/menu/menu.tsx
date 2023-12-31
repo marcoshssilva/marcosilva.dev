781264eda7c3c42d8a68140bc9a0988928b379c7
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-12-31 18:47:44 -0300

    Remove buttons and grow up font-size from menu

--- src/app/components/menu/menu.tsx
+++ src/app/components/menu/menu.tsx
@@ -15,33 +15,15 @@ export default function MenuComponent() {
     <AppBar position="fixed" color="transparent" sx={{ boxShadow: 'none' }}>
       <Container>
         <Toolbar>
-          <Typography variant="h6" component="a" href="/">
+          <Typography variant="h5" component="a" href="/">
             marcoshssilva
           </Typography>
-          <Typography variant="h6" component="a" href="/" color='primary'>
+          <Typography variant="h5" component="a" href="/" color='primary'>
             .com.br
           </Typography>
 
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}></Box>
-
-          <IconButton
-            size="large"
-            aria-label="Redirect to Github"
-            color="inherit"
-            target="_blank"
-            href="https://github.com/marcoshssilva">
-                <GitHubIcon />
-          </IconButton>
-
-          <IconButton
-            size="large"
-            aria-label="Redirect to LinkedIn"
-            color="inherit"
-            target="_blank"
-            href="https://www.linkedin.com/in/marcos-henrique-santana/">
-                <LinkedInIcon />
-          </IconButton>
 
         </Toolbar>
       </Container>
