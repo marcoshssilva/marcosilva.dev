664084910027200108f577c2b8e12e8610b55c77
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-15 00:22:00 -0300

    Change text of header-hello.tsx

--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -3,18 +3,9 @@ import { Box, Typography } from "@mui/material";
 export default function HeaderHelloComponent() {
   return <>
     <Box className="flex flex-col">
-
-      <Typography variant="h1" sx={{ fontWeight: 600, fontSize: '4rem' }}>Olá, eu sou</Typography>
-      <Typography variant="h1" sx={{ fontWeight: 600, fontSize: '4rem', color: 'var(--color-yellow);' }}>Marcos Henrique.</Typography>
-      <Typography variant="h1" sx={{ fontWeight: 300, fontSize: '2.12rem', marginBottom: '0.75rem' }}>
-        Desenvolvedor de aplicações
+      <Typography variant="h1" sx={{fontWeight: 600, fontSize: '4.12rem', textAlign: 'left'}}>
+        Construir tecnologia<br/> para um novo <br/>mundo <br/> faz o meu propósito.
       </Typography>
-
-      <Typography sx={{ paddingY: '0.5rem', paddingRight: '1rem' }} >
-        Apenas um mero desenvolvedor com foco em backend, frontend, nuvem e IA.<br/>
-        Sinta-se confortavel para entrar em contado comigo.
-      </Typography>
-
     </Box>
     </>;
 }
