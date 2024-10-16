9fd47666bd9cea62c5e057ecf716e380f751d55c
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-16 01:31:21 -0300

    Optimize image to different sizes

--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -6,12 +6,12 @@ import {
 
 export default function HeaderHelloComponent() {
   return <>
-    <Box id="header-hello" className="relative">
+    <Box id="header-hello" className="relative box-section">
       <Container className={"lg:pt-0 container-full-with-menu grid px-6 grid-cols-1 md:grid-cols-12 gap-5"}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/images/headers/me.png"
-             className="absolute bottom-0 right-0 w-[778px] h-auto"
-             alt=""
+             className="absolute bottom-0 right-0 h-auto sm:w-[80vw] md:w-[60vw] 2xl:w-[40vw]"
+             alt="illustrated image from myself"
              loading="lazy"
              decoding="async" />
       </Container>
