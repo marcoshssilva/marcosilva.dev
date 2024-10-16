467a168701bb02b9365521c2fd8e66ea2ca85fea
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-15 23:59:33 -0300

    Update header-hello.tsx

--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -1,11 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import React from "react";
+import {
+  Box,
+  Container,
+} from "@mui/material";
 
 export default function HeaderHelloComponent() {
   return <>
-    <Box className="flex flex-col">
-      <Typography variant="h1" sx={{fontWeight: 600, fontSize: '4.12rem', textAlign: 'left'}}>
-        Construir tecnologia<br/> para um novo <br/>mundo <br/> faz o meu propósito.
-      </Typography>
+    <Box id="header-hello" className="relative">
+      <Container className={"lg:pt-0 container-full-with-menu grid px-6 grid-cols-1 md:grid-cols-12 gap-5"}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/images/headers/me-right.png"
+             className="absolute bottom-0 right-0 w-[778px] h-auto"
+             alt=""
+             loading="lazy"
+             decoding="async" />
+      </Container>
     </Box>
     </>;
 }
