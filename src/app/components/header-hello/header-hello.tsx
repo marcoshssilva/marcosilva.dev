f7fe158a49d86afe1c8004826750f9c06ef4ff1d
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-16 01:34:43 -0300

    Refactor code to remove unused css classes

--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -7,7 +7,7 @@ import {
 export default function HeaderHelloComponent() {
   return <>
     <Box id="header-hello" className="relative box-section">
-      <Container className={"lg:pt-0 container-full-with-menu grid px-6 grid-cols-1 md:grid-cols-12 gap-5"}>
+      <Container className={"lg:pt-0 h-screen min-h-[740px] grid px-6 grid-cols-1 md:grid-cols-12 gap-5"}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/images/headers/me.png"
              className="absolute bottom-0 right-0 h-auto sm:w-[80vw] md:w-[60vw] 2xl:w-[40vw]"
