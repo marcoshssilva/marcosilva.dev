c275e60c65f27a3a54b83de54211b301b524ecc6
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-16 00:00:11 -0300

    Refactor page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
-import { Box, Container } from "@mui/material";
+import React from "react";
 
 import MenuComponent from "./components/menu/menu";
 import HeaderHelloComponent from "./components/header-hello/header-hello";
-import HeaderGroupButtonsComponent from "./components/header-group-buttons/header-group-buttons";
-import HeaderContactMe from "./components/header-contact-me/header-contact-me";
-
 
 export default function Home() {
   return (
     <>
         <MenuComponent />
-        <Container className={"min-h-screen pt-[72px] lg:pt-0"} sx={{ position: 'relative' }}>
-          <Box className={"grid absolute md:bottom-1/2 md:translate-y-1/2 px-6 grid-cols-1 md:grid-cols-12 gap-5"}>
-            <Box className={'col-span-1 md:col-span-7 h-full'}>
-              <HeaderHelloComponent />
-              <HeaderGroupButtonsComponent />
-            </Box>
-            <Box className={'col-span-1 md:col-span-5 h-full'}>
-              <HeaderContactMe />
-            </Box>
-          </Box>
-        </Container>
+        <HeaderHelloComponent />
     </>
   );
 }
