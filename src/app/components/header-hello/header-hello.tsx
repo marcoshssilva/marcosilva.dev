b572af07b1efcb9fea8cddf43681f53e37df15ba
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-16 00:02:33 -0300

    Fixed image source

--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -9,7 +9,7 @@ export default function HeaderHelloComponent() {
     <Box id="header-hello" className="relative">
       <Container className={"lg:pt-0 container-full-with-menu grid px-6 grid-cols-1 md:grid-cols-12 gap-5"}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/images/headers/me-right.png"
+        <img src="/images/headers/me.png"
              className="absolute bottom-0 right-0 w-[778px] h-auto"
              alt=""
              loading="lazy"
