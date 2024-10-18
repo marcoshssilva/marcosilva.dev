2821792ce48a2ee9f2cfa15437ef661af2bf8ec5
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-18 13:33:50 -0300

    enable dark mode into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+'use client'
 import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 import MenuComponent from "./components/menu/menu";
 import HeaderHelloComponent from "./components/header-hello/header-hello";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 export default function Home() {
   return (
     <>
+      <ThemeProvider theme={darkTheme}>
         <MenuComponent />
         <HeaderHelloComponent />
+      </ThemeProvider>
     </>
   );
 }
