6ef0c2c14beb4d61f1eb1318ad82d3610953c56c
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-18 13:40:43 -0300

    Change header-hello.tsx texts

--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -1,33 +1,56 @@
 import React from "react";
+import Image from "next/image";
 import {
   Box,
-  Container, Typography,
+  Container,
+  Divider,
+  Typography,
+  Button
 } from "@mui/material";
 
 export default function HeaderHelloComponent() {
   return <>
-    <Box id="header-hello" className="relative box-section">
-      <Container className={"lg:pt-0 h-screen min-h-[740px] grid px-6 grid-cols-1 md:grid-cols-12 gap-5"}>
+    <Box component={'header'} id="header-hello" className="relative box-section">
+      <Container className={"lg:pt-0 min-h-screen grid grid-cols-1 md:grid-cols-12 gap-5 pt-[97px]"}>
+        <Box className={'md:sticky md:top-1/2 md:bottom-1/2 md:-translate-y-1/2 mt-12'}>
+          <Typography variant="h1" sx={{
+            fontWeight: 600,
+            fontSize: { xs: '1.20rem', md: '2.12rem' },
+            textAlign: { xs: 'center', md: 'left' },
+            paddingTop: '3rem'
+          }}>
+            Bem-vindo(a) ao meu portifólio
+          </Typography>
+          <Image
+            className={'my-0 mx-auto lg:mx-0'}
+            src={"/images/headers/Title.png"}
+            alt={"DEV"}
+            width={720} height={178}
+            style={{ paddingTop: '1.2rem', paddingBottom: '.1rem', }}
+          />
+          <Typography variant="h1" sx={{
+            fontWeight: 300,
+            fontSize: { xs: '1.20rem', md: '2.12rem' },
+            textAlign: { xs: 'center', md: 'left' },
+            paddingBottom: '1.2rem'
+          }}>
+            Desenvolvedor <span>CLOUD</span>
+          </Typography>
+          <Typography sx={{
+            fontFamily: "'Open Sans', sans-serif",
+            textAlign: { xs: 'center', md: 'left' },
+          }}>
+            {'\"'}As pessoas não sabem o que querem, até mostrarmos a elas.{'\"'} <br/>
+            &nbsp; - Steve Jobs
+          </Typography>
+          <Divider sx={{ marginY: '1.5rem', width: { md: '33%' } }} color={'ffc409'}></Divider>
+        </Box>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/images/headers/me.png"
-             className="absolute bottom-0 right-0 h-auto sm:w-[80vw] md:w-[60vw] 2xl:w-[40vw]"
+             className="md:absolute bottom-0 right-0 h-auto w-[130vh] sm:w-[80vw] sm:translate-x-[8rem] md:w-[60vw] 2xl:w-[40vw] translate-x-4 md:translate-x-0 -z-10"
              alt="illustrated image from myself"
              loading="lazy"
              decoding="async" />
-
-        <Box className="flex flex-col">
-          <Typography variant="h1" sx={{ fontWeight: 600, fontSize: '4rem' }}>Olá, eu sou</Typography>
-          <Typography variant="h1" sx={{ fontWeight: 600, fontSize: '4rem', color: 'var(--color-yellow);' }}>Marcos Henrique.</Typography>
-          <Typography variant="h1" sx={{ fontWeight: 300, fontSize: '2.12rem', marginBottom: '0.75rem' }}>
-            Desenvolvedor de aplicações
-          </Typography>
-
-          <Typography sx={{ paddingY: '0.5rem', paddingRight: '1rem' }} >
-            Apenas um mero desenvolvedor com foco em backend, frontend, nuvem e IA.<br/>
-            Sinta-se confortavel para entrar em contado comigo.
-          </Typography>
-        </Box>
-
       </Container>
     </Box>
     </>;
