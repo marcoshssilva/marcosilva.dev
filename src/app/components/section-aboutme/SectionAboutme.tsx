02b1155a8bb9ccd49b7c713c37277eaee71e4cd8
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-27 19:49:30 -0300

    Change style from SectionAboutme title

--- src/app/components/section-aboutme/SectionAboutme.tsx
+++ src/app/components/section-aboutme/SectionAboutme.tsx
@@ -6,9 +6,14 @@ import {
 export default function SectionAboutme() {
   return <>
     <Box component={'section'} id="section-aboutme" className="relative box-section background-aboutme">
-      <Container className='min-h-screen pt-[127px]'>
-        <Typography align='center' className='section-aboutme-title px-4'>
-          Marcos Henrique
+      <Container className='min-h-screen pt-[97px]'>
+        <Typography
+          className='section-aboutme-title'
+          variant='h1'
+          align='left'
+          sx={{ marginTop: '1.2rem', marginBottom: '1rem', marginX: '1rem' }}
+        >
+          #Sobre Mim
         </Typography>
         <Box className='grid grid-cols-1 md'></Box>
       </Container>
