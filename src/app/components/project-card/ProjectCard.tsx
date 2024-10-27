fe16addda9f1f15f24e6514109cb8de003bcdcd8
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-10-26 23:23:58 -0300

    Style effects on ProjectCard.tsx

--- src/app/components/project-card/ProjectCard.tsx
+++ src/app/components/project-card/ProjectCard.tsx
@@ -1,38 +1,49 @@
 import {
   Box,
-  Typography
+  Typography,
+  IconButton,
 } from "@mui/material";
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LanguageIcon from '@mui/icons-material/Language';
 
 import { ProjectCardDataProps } from "@/app/types";
 
 export default function ProjectCard ({ project }: ProjectCardDataProps) {
   return <>
     <Box
-      className='project-card h-full'
+      className='project-card h-full relative hover:border-yellow-200 hover:border hover:-translate-y-[3px] transition-all duration-200 hover:shadow-lg hover:shadow-[#483285DD]'
       sx={{
         backgroundColor: 'var(--color-black-tint)',
         padding: '1rem',
         borderRadius: '16px'
     }}>
-      {/*<Box sx={{ backgroundColor: '#00cec9', borderRadius: '16px' }}>*/}
-      {/*  <svg viewBox="0 0 200 200" width="100%" height="100%">*/}
-      {/*    <circle cx="100" cy="100" r="80" fill="#00cec9"></circle>*/}
-      {/*    <circle cx="100" cy="100" r="60" fill="#2d3436"></circle>*/}
-      {/*    <circle cx="100" cy="100" r="40" fill="#fd79a8"></circle>*/}
-      {/*  </svg>*/}
-      {/*</Box>*/}
       <Typography variant='h6' className='project-card-title'>
         { project.title }
       </Typography>
       <Typography>
         { project.description }
       </Typography>
-      <Box className='project-card-tags'>
+      <Box className='project-card-tags pb-8'>
         { project.tags.map((tag, index) => <span key={index} className='project-card-tag'> {tag} </span>) }
       </Box>
-      {/*<Box>*/}
-      {/*  { project.links.map((link, index) => <a key={index} target='_blank' href={link.href}> {link.icon} </a>) }*/}
-      {/*</Box>*/}
+      <Box className='my-4 absolute bottom-0'>
+        { project.links.map((link, index) => <IconButton
+          key={index}
+          target='_blank'
+          href={link.href}>
+          <ProjectCardButtonIcon icon={link.icon} />
+        </IconButton>) }
+      </Box>
     </Box>
   </>
+}
+
+function ProjectCardButtonIcon({ icon }: { icon: string }) {
+  switch (icon){
+    case 'github':
+      return (<GitHubIcon />)
+    default:
+      return (<LanguageIcon />)
+  }
 }
