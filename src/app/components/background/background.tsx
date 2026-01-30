ec9978f3b5c7423845b4f74c6cc6657c96fe1694
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-01-30 19:40:10 -0300

    chore: update alt text for background images in `background.tsx`

--- src/app/components/background/background.tsx
+++ src/app/components/background/background.tsx
@@ -3,14 +3,14 @@ export default function Background(props: Record<string, any>) {
     {/* eslint-disable-next-line @next/next/no-img-element */}
     <img src="/images/headers/Laptop.png"
          className="block fixed bottom-0 left-0 h-auto md:w-[70%] -z-10"
-         alt="illustrated image from myself"
+         alt="laptop"
          loading="lazy"
          decoding="async" />
 
     {/* eslint-disable-next-line @next/next/no-img-element */}
     <img src="/images/headers/me.png"
          className="fixed bottom-0 right-0 h-auto w-[130vh] sm:w-[80vw] sm:translate-x-[8rem] md:w-[60vw] 2xl:w-[40vw] translate-x-[150px] md:translate-x-0 -z-10"
-         alt="illustrated image from myself"
+         alt="myself render as carton"
          loading="lazy"
          decoding="async" />
   </div>
