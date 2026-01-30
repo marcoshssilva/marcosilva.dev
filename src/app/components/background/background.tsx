d78b56382b5f958f846e782559da75859cca7075
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-01-30 17:15:25 -0300

    feat: extract background images to a new reusable component

---
+++ src/app/components/background/background.tsx
@@ -0,0 +1,19 @@
+import {Container} from "@mui/material";
+
+export default function Background(props: Record<string, any>) {
+  return <div className={'background'}>
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img src="/images/headers/Laptop.png"
+         className="block fixed bottom-0 left-0 h-auto md:w-[70%] -z-10"
+         alt="illustrated image from myself"
+         loading="lazy"
+         decoding="async" />
+
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img src="/images/headers/me.png"
+         className="fixed bottom-0 right-0 h-auto w-[130vh] sm:w-[80vw] sm:translate-x-[8rem] md:w-[60vw] 2xl:w-[40vw] translate-x-[150px] md:translate-x-0 -z-10"
+         alt="illustrated image from myself"
+         loading="lazy"
+         decoding="async" />
+  </div>
+}
--- src/app/components/header-hello/header-hello.tsx
+++ src/app/components/header-hello/header-hello.tsx
@@ -61,18 +61,6 @@ export default function HeaderHelloComponent() {
             />
           </Box>
         </Box>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/images/headers/Laptop.png"
-             className="block fixed bottom-0 left-0 h-auto md:w-[70%] -z-10"
-             alt="illustrated image from myself"
-             loading="lazy"
-             decoding="async" />
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/images/headers/me.png"
-             className="fixed bottom-0 right-0 h-auto w-[130vh] sm:w-[80vw] sm:translate-x-[8rem] md:w-[60vw] 2xl:w-[40vw] translate-x-[150px] md:translate-x-0 -z-10"
-             alt="illustrated image from myself"
-             loading="lazy"
-             decoding="async" />
       </Container>
     </Box>;
 }
